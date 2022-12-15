3b4636866e2dd08274996d13457e676e88ebc50b
Tal Hason <97454240+tal-hason@users.noreply.github.com>
2022-12-15 16:39:24 +0000

    updated the app.js application

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ const app = express();
 const router = express.Router();
 var port = process.env.PORT || 8080;
 
-router.get('/', function (req, res) {
-  res.send(`Hello World!`);
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/html/index.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
 app.use('/', router);
