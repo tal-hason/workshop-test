7c0995f5cd835d36bd46a8d63f7aae4827dbe2ff
Tal Hason <97454240+tal-hason@users.noreply.github.com>
2022-12-15 17:05:41 +0000

    fix path

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const router = express.Router();
 var port = process.env.PORT || 8080;
 
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/html/index.html'));
+    res.sendFile(path.join(__dirname+'html/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
